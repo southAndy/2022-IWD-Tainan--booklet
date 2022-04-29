350605a9f81006194fbc4bbec1020a85de97dd00
southAndy <zatchbell.1206@gmail.com>
2022-04-29 11:36:00 +0800

    feat: addscrollEvent

---
+++ js/scrollEffect.js
@@ -0,0 +1,17 @@
+// window.addEventListener("scroll",function(){
+//     // console.log("hi",this.win)
+//     const arrowIcon = this.document.querySelector(".invisible")
+    
+//     console.log("scrollY",window.scrollY)
+//     console.log("innerHeight",window.innerHeight)
+//     // arrowIcon.classList.toggle("invisible")
+
+//     if(this.window.scrollY === this.window.innerHeight){
+//         console.log("enter")
+//     }
+//     // arrowIcon.classList.toggle("invisible")    
+//     // console.log(arrowIcon)
+//     // while(this.window.scrollY === this.window.innerHeight){
+//     //     arrowIcon.classList.toggle("invisible")
+//     // }
+// })
